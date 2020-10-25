52603b74fe27d9cefc3bb803b551f04c8da43989
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-26 00:28:24 +0300

    project-update

--- cityYelpApp/src/pages/RestaurantList.js
+++ cityYelpApp/src/pages/RestaurantList.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView, View, Text, FlatList } from 'react-native';
-import { RestaurantItem } from '../components';
+import { RestaurantItem, SearchBar } from '../components';
+
+let originalList = [];
 
 const RestaurantList = (props) => {
     const [restaurantList, setRestaurantList] = useState([]);
@@ -18,6 +20,7 @@ const RestaurantList = (props) => {
             })
             .then(response => {
                 setRestaurantList(response.data.restaurants);
+                originalList = [...response.data.restaurants];
             })
     }
 
@@ -29,15 +32,34 @@ const RestaurantList = (props) => {
         return (
             <RestaurantItem
                 restaurant={item}
+                onSelect={() => props.navigation.navigate('Details', { selectedRestaurant: item })}
             />
         )
     }
 
+    function searchRestaurant(search) {
+        const filteredRestaurants = originalList.filter(restaurant => {
+            const text = search.toUpperCase();
+            const restaurantName = restaurant.name.toUpperCase();
+
+            return restaurantName.indexOf(text) > -1;
+        })
+
+        setRestaurantList(filteredRestaurants);
+    }
+
     return (
-        <SafeAreaView>
-            <View>
-                <Text>{selectedCity}</Text>
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
+                <View>
+                    <Text style={{ margin: 5, fontWeight: 'bold', fontSize: 30 }}>{selectedCity} Restaurants</Text>
+                    <SearchBar
+                        placeholder="Search a restaurant..."
+                        onSearch={(value) => searchRestaurant(value)}
+                    />
+                </View>
                 <FlatList
+                    keyExtractor={(_, index) => index.toString()}
                     data={restaurantList}
                     renderItem={renderRestaurants}
                 />
