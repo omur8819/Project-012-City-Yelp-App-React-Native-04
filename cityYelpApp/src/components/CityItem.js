225782290156e4cb50bc4aacff0640d33455fc8c
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-25 19:17:53 +0300

    city-item-component-added

---
+++ cityYelpApp/src/components/CityItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const CityItem = (props) => {
+    return (
+        <TouchableOpacity style={styles.container}>
+            <Text style={styles.text}>{props.cityName}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export { CityItem };
+
+
+const styles = StyleSheet.create({
+    container:{
+        padding: 10,
+        alignItems: "center",
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: '300',
+    },
+})
--- cityYelpApp/src/pages/CityList.js
+++ cityYelpApp/src/pages/CityList.js
@@ -1,36 +1,38 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import {SafeAreaView, View, Text, FlatList} from 'react-native';
+import { SafeAreaView, View, Text, FlatList } from 'react-native';
+
+import { CityItem } from '../components'
 
 const CityList = (props) => {
     const [cityList, setCityList] = useState([]);
 
-// ASNC-AWAIT
+    // ASYNC-AWAIT 
     const fetchCityData = async () => {
         const { data } = await axios.get("https://opentable.herokuapp.com/api/cities");
-        setCityList(data.cities)
+        setCityList(data.cities);
     }
 
     useEffect(() => {
         fetchCityData();
     }, [])
 
-    const renderCities = ({item}) => <Text>{item}</Text>
+    const renderCities = ({ item }) => <CityItem cityName={item} />
 
-    return(
-    <SafeAreaView>
-        <View>
-            <FlatList 
-                keyExtractor={(_, index) => index.toString()}
-                data={cityList}
-                renderItem={renderCities}
-            />
-        </View>
-    </SafeAreaView>
+    const renderSeperator = () => <View style={{borderWidth: 1, borderColor: '#e0e0e0'}} />
+
+    return (
+        <SafeAreaView>
+            <View>
+                <FlatList
+                    keyExtractor={(_, index) => index.toString()}
+                    data={cityList}
+                    renderItem={renderCities}
+                    ItemSeparatorComponent={renderSeperator}
+                />
+            </View>
+        </SafeAreaView>
     )
 }
 
-export {CityList};
-
-
-
+export { CityList };
