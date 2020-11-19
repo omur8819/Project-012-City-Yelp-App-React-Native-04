7d8da3f3f2eaaa5fdee5cf605bcb608918d20104
omur8819 <omur8819@gmail.com>
2020-11-20 00:04:07 +0300

    RestaurantList completed

--- cityYelpApp/src/pages/RestaurantList.js
+++ cityYelpApp/src/pages/RestaurantList.js
@@ -1,34 +1,49 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
-
-import { RestaurantItem } from './index';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, View, Text, FlatList } from 'react-native';
+import { RestaurantItem } from '../components';
 
 const RestaurantList = (props) => {
-    const { selectedCity } = props.route.params
+    const [restaurantList, setRestaurantList] = useState([]);
+    const { selectedCity } = props.route.params;
+
 
     const fetchRestaurants = () => {
-        axios.get('https://opentable.herokuapp.com/api/restaurants', 
-            { 
+        axios.get(
+            'http://opentable.herokuapp.com/api/restaurants',
+            {
                 params: {
-                    "city": selectedCity
-            }
-        })
-        .then(response => console.log(response))
-        .catch(err => console.log(err))
+                    city: selectedCity
+                }
+            })
+            .then(response => {
+                setRestaurantList(response.data.restaurants);
+            })
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchRestaurants();
     }, [])
+
+    const renderRestaurants = ({ item }) => {
+        return (
+            <RestaurantItem
+                restaurant={item}
+            />
+        )
+    }
 
     return (
         <SafeAreaView>
             <View>
                 <Text>{selectedCity}</Text>
+                <FlatList
+                    data={restaurantList}
+                    renderItem={renderRestaurants}
+                />
             </View>
         </SafeAreaView>
     )
 }
 
-export { RestaurantList };
+export { RestaurantList }
